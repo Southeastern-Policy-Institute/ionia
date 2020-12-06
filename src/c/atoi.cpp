8e07cf02f5f13b8c0d0beed63508396187325f75
Loanfish <claudia@southeasternpolicyinstitute.com>
2020-12-05 19:10:51 -0500

    Update atoi.cpp

--- src/c/atoi.cpp
+++ src/c/atoi.cpp
@@ -3,8 +3,6 @@
  */
 
 # include <stdlib.h>
-# include <ionia/bitmask.hpp> // bitmask_t<uint32_t>::BTS()
-# include <ionia/string.hpp>  // ionia::string::strlen()
 
 extern "C" __attribute__ ((nonnull, nothrow))
 __INT32_TYPE__ std::atoi (const char* str) {
@@ -12,22 +10,30 @@ __INT32_TYPE__ std::atoi (const char* str) {
   __INT32_TYPE__ output = 0;
 
   // Internal routine to produce powers of 10
-  auto pow10 = [](__UINT32_TYPE__ val) {
+  auto pow10 = [] (__UINT32_TYPE__ val) -> __UINT32_TYPE__ {
     __UINT32_TYPE__ __output = 1;
     while (val--)
       __output *= 10;
     return __output;
   };
 
+  // Internal strlen (to avoid cross-cutting concerns)
+  auto strlen = [] (const char* str) -> size_t {
+    size_t __output = 0;
+    while (*str++)
+      __output++;
+    return __output;
+  };
+
   // Make output negative if called for
   if (*str == '-') {
-    output = bitmask_t<uint32_t>::BTS (output, 31);
+    output |= (1U << 31);
     ++str;
   };
 
   // Parse the digits from right to left
-  for ( __SIZE_TYPE__ i = ionia::string::strlen (str) - 1, j = 0
-      ; j < ionia::string::strlen (str)
+  for ( __SIZE_TYPE__ i = strlen (str) - 1, j = 0
+      ; j < strlen (str)
       ; i--, j++)
     output += ('0' - str[i]) * pow10 (j);
 
